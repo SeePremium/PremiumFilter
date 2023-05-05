c4c49c7e950b9cd57c8a05ec924e9a9270984c7d
nikhilvishwa7 <123230101+nikhilvishwa7@users.noreply.github.com>
2023-05-05 20:55:11 +0530

    Update info.py

--- info.py
+++ info.py
@@ -71,7 +71,6 @@ FILE_REQ_CHANNEL = int(environ.get('FILE_REQ_CHANNEL', LOG_CHANNEL))
 
 SHORTNER_SITE =  environ.get('SHORTNER_SITE', 'omegalinks.in') #Put Only Shortner Site domain don't put like this https://tnlink.in/
 
-SHORTNER_API =  environ.get('SHORTNER_API', 'bf56e643ddbd84748b7567d820d8c51b44f0bca7
-')
+SHORTNER_API =  environ.get('SHORTNER_API', 'bf56e643ddbd84748b7567d820d8c51b44f0bca7')
 
 AUTO_DELETE =  environ.get('AUTO_DELETE', 'True')
