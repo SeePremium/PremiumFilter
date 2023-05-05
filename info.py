0839d705a6d0141bf1710868aa14526aed19d420
nikhilvishwa7 <123230101+nikhilvishwa7@users.noreply.github.com>
2023-05-05 11:15:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -65,14 +65,14 @@ LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 
 
 #Verify Features
-VERIFY = bool(environ.get('VERIFY', True))
+HOW_TO_DOWNLOAD =  environ.get('HOW_TO_DOWNLOAD', 'https://t.me/VisionBackup/331')
 
-AUTO_DELETE_SECONDS = int(environ.get('AUTO_DELETE_SECONDS', 60))
+AUTO_DELETE_SECONDS = int(environ.get('AUTO_DELETE_SECONDS', 100))
 
 FILE_REQ_CHANNEL = int(environ.get('FILE_REQ_CHANNEL', LOG_CHANNEL))
-
-HOW_TO_VERIFY = environ.get("HOW_TO_VERIFY", "https://t.me/VisionBackup/331")
 
 SHORTNER_SITE =  environ.get('SHORTNER_SITE', 'mdisklink.link') #Put Only Shortner Site domain don't put like this https://tnlink.in/
 
 SHORTNER_API =  environ.get('SHORTNER_API', '75e830d6e6bb44e5a2d13c1754ba7487f03a3336')
+
+AUTO_DELETE =  environ.get('AUTO_DELETE', 'True')
