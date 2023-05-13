dbb295ce0a7d3e109f70d5608c5c9c1b977a1084
nikhilvishwa7 <123230101+nikhilvishwa7@users.noreply.github.com>
2023-05-13 18:00:16 +0530

    Update info.py

--- info.py
+++ info.py
@@ -69,8 +69,8 @@ AUTO_DELETE_SECONDS = int(environ.get('AUTO_DELETE_SECONDS', 100))
 
 FILE_REQ_CHANNEL = int(environ.get('FILE_REQ_CHANNEL', LOG_CHANNEL))
 
-SHORTNER_SITE =  environ.get('SHORTNER_SITE', 'omegalinks.in') #Put Only Shortner Site domain don't put like this https://tnlink.in/
+SHORTNER_SITE =  environ.get('SHORTNER_SITE', 'tinyfy.in') #Put Only Shortner Site domain don't put like this https://tnlink.in/
 
-SHORTNER_API =  environ.get('SHORTNER_API', 'bf56e643ddbd84748b7567d820d8c51b44f0bca7')
+SHORTNER_API =  environ.get('SHORTNER_API', '645f3cb39f7fd504c3de8e6adb71722fe5c3d8d0')
 
 AUTO_DELETE =  environ.get('AUTO_DELETE', 'True')
