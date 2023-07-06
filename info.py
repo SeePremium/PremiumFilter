c4cdfce09f6d7a9d115667a415a5089d06e081d9
jngohel <136894773+jngohel@users.noreply.github.com>
2023-07-06 12:31:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -69,8 +69,8 @@ AUTO_DELETE_SECONDS = int(environ.get('AUTO_DELETE_SECONDS', 100))
 
 FILE_REQ_CHANNEL = int(environ.get('FILE_REQ_CHANNEL', LOG_CHANNEL))
 
-SHORTNER_SITE =  environ.get('SHORTNER_SITE', 'mdisklink.link') 
+SHORTNER_SITE =  environ.get('SHORTNER_SITE', 'Ziplinker.net ') 
 
-SHORTNER_API =  environ.get('SHORTNER_API', '75e830d6e6bb44e5a2d13c1754ba7487f03a3336')
+SHORTNER_API =  environ.get('SHORTNER_API', 'b2488d7cb39a3bda4f66123a3f67cb773af054fb')
 
 AUTO_DELETE =  environ.get('AUTO_DELETE', 'True')
